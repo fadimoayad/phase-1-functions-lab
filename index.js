ab5b09eb7e974f1815adf4510f8cbae7763fa5f3
fadimoayad <fadimoayad99@yahoo.com>
2023-01-13 15:10:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+if (distance>42){
+return distance-42;
+}else if(distance<42){
+    return 42-distance;
+}
+}
+
+function distanceFromHqInFeet(distance){
+let result=distanceFromHqInBlocks(distance)
+return result *264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let result;
+    if(start<destination){
+         result=destination-start;
+    }else{
+         result=start-destination;
+
+    }
+
+return result*264;
+}
+
+
+function calculatesFarePrice(start, destination){
+let startfeet=start*264;
+let destinationfeet=destination*264;
+let distance;
+if(startfeet<destinationfeet){
+    distance=destinationfeet-startfeet;
+}else{
+    distance=startfeet-destinationfeet;
+
+}
+   if(distance<=400){
+        return 0;
+    }else if(distance>400 && distance<2000){
+        distance=distance-400
+        return distance *0.02;
+    }else if(distance>2000 &&distance<2500){
+        return 25;
+    }else if(distance>2500){
+        return "cannot travel that far"
+    }
+
+}
